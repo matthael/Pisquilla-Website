bd360c9fffb73d091fc99938a2b523588fc32c4e
Matheus Ponte <mfponte2006@gmail.com>
2023-03-30 21:07:57 -0300

    Feature:
    add: Navbar with their styles and responsive feature
    
    Fix: main.jsx turn into React 18 and reorganize the assets files

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,75 @@
-import React from 'react';
+import React, {useState} from 'react';
+
+import { FaFacebook, FaInstagram } from "react-icons/fa";
+import  { SiIfood } from "react-icons/si"
+import { GiHamburgerMenu } from 'react-icons/gi'
+import { MdOutlineRestaurantMenu } from 'react-icons/md'
+
+import images from '../../constants/images'
 
 import './Navbar.css';
 
-const Navbar = () => (
-  <div>
-    Navbar
-  </div>
+const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false)
+
+  return (
+  <nav className="app__navbar"> 
+    <div className="app__navbar-logo">
+      <img src={""} alt="app logo" />
+      <h1 className="app__navbar-logo-name"> Pisquilla</h1> 
+    </div>
+    <ul className="app__navbar-links"> 
+      <li className="p__opensans"><a href="#home">Home</a></li>
+      <li className="p__opensans"><a href=" #about">Sobre</a></li>
+      <li className="p__opensans"><a href="#menu">Menu</a></li>
+      <li className="p__opensans"><a href="#tesm">Avaliações</a></li>
+      <li className="p__opensans"><a href="#Cont">Contato</a></li>
+      <li className="p__opensans"><a href="#Find"> Encontre-nós</a></li>
+    </ul>
+    <div className="app__navbar-social-links">
+    <ul >
+      <li>
+        <a href="" target="blank">
+          <SiIfood />
+        </a>
+      </li>
+      <li>
+        <a
+          href=""
+          target="blank"
+        >
+          <FaInstagram />
+        </a>
+      </li>
+      <li>
+        <a
+          href=""
+          target="blank"
+        >
+          <FaFacebook />
+        </a>
+      </li>
+    </ul>
+    </div>
+    <div className="app__navbar-smallscreen">
+      <GiHamburgerMenu color="#fff" fontSize={27} onClick={() => setToggleMenu(true)}/>
+      
+      {toggleMenu && (
+      <div className="app__navbar-smallscreen_overlay flex__center slide-bottom">
+        <MdOutlineRestaurantMenu fontSize={27} className="overlay__close" onClick={() => setToggleMenu(false)}/>
+        <ul className="app__navbar-smallscreen_links"> 
+          <li className="p__opensans"><a href="#home">Home</a></li>
+          <li className="p__opensans"><a href=" #about">Sobre</a></li>
+          <li className="p__opensans"><a href="#menu">Menu</a></li>
+          <li className="p__opensans"><a href="#tesm">Avaliações</a></li>
+          <li className="p__opensans"><a href="#Cont">Contato</a></li>
+          <li className="p__opensans"><a href="#Find"> Encontre-nós</a></li>
+        </ul>
+      </div>
+      )}
+    </div>
+  </nav>
 );
+}
 
 export default Navbar;
--- src/main.jsx
+++ src/main.jsx
@@ -4,5 +4,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+)
 
